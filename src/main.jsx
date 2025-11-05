d01c28cd4c14f3cb8509845ddc3f9ce6a7c7fb4a
kazetama <tamaketuahimpunan@gmail.com>
2025-11-05 14:50:15 +0700

    refactor: restructure folders and add React Router with path alias

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+
+import Wellcome from "@/pages/Wellcome.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Wellcome />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
